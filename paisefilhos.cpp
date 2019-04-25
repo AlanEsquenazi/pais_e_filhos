175da3bd0eb9a7419dd43671b937afd87687a5e2
Alan <esquenazialan@gmail.com>
2019-04-24 22:11:45 -0300

    adiciona capacidades de pesquisa de aluno a partir do responsavel

--- paisefilhos.cpp
+++ paisefilhos.cpp
@@ -10,7 +10,7 @@ void adicionar(string nome, string aluno, string pai, string mae){
     arquivo.close();
 }
 */
-void pesquisar(string nome, string aluno){
+void pesquisar_aluno(string nome, string aluno){
     ifstream arquivo;
     arquivo.open(nome);
     string line;
@@ -34,6 +34,54 @@ void pesquisar(string nome, string aluno){
     }
     arquivo.close();
 }
+void pesquisar_mae(string nome, string mae){
+    ifstream arquivo;
+    arquivo.open(nome);
+    string line;
+    while(getline(arquivo, line)){
+        string apm[3];
+        int conta = 0;
+        for(int i=0;i<sizeof(line);i++){
+            if(line[i]==';'){
+                break;
+            }else{
+                if(!(line[i]==':')){
+                    apm[conta] +=line[i];
+                }else{
+                    conta++;
+            }
+            }
+        }
+        if(apm[2]==mae){
+            cout<<"Aluno: "<<apm[0]<<'\n';
+        }
+    }
+    arquivo.close();
+}
+void pesquisar_pai(string nome, string pai){
+    ifstream arquivo;
+    arquivo.open(nome);
+    string line;
+    while(getline(arquivo, line)){
+        string apm[3];
+        int conta = 0;
+        for(int i=0;i<sizeof(line);i++){
+            if(line[i]==';'){
+                break;
+            }else{
+                if(!(line[i]==':')){
+                    apm[conta] +=line[i];
+                }else{
+                    conta++;
+            }
+            }
+        }
+        if(apm[1]==pai){
+            cout<<"Aluno: "<<apm[0]<<'\n';
+        }
+    }
+    arquivo.close();
+}
 void input(string entrada){
     string pai, mae, aluno;
     /*
@@ -42,11 +90,17 @@ void input(string entrada){
         adicionar("paisefilhos.txt", aluno, pai, mae);
     }else 
     */
-   if(entrada =="pesquisar"){
+   if(entrada =="pesquisaraluno"||entrada =="pesquisar aluno"||entrada =="pesquisar_aluno"||entrada =="Pesquisaraluno"||entrada =="P esquisar aluno"||entrada =="Pesquisar Aluno"){
         cin>>aluno;
-        pesquisar("paisefilhos.txt", aluno);
+        pesquisar_aluno("paisefilhos.txt", aluno);
+    }else if(entrada=="pesquisarmae"||entrada=="pesquisar mae"||entrada=="Pesquisarmae"||entrada=="Pesquisar mae"||entrada=="Pesquisar Mae"){
+        cin>>mae;
+        pesquisar_mae("paisefilhos.txt", mae);
+    }else if(entrada=="pesquisarpai"||entrada=="pesquisar pai"||entrada=="Pesquisarpai"||entrada=="Pesquisar pai"||entrada=="Pesquisar Pai"){
+        cin>>pai;
+        pesquisar_pai("paisefilhos.txt", pai);
     }else{
-        cout<<"Comando nao reconhecido";
+        cout<<"Comando nao reconhecido; favor digitar pesquisaraluno, pesquisarmae ou pesquisarpai"<<'\n';
     }
 }
 int main(){
